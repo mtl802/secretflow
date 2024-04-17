d767bc1ae737b65b17606a56ee27a5349e942da3
伦哥 <7493863+mtl802@user.noreply.gitee.com>
2024-04-17 17:31:04 +0800

    增加3方PSI组件

--- docker/dev/entry.sh
+++ docker/dev/entry.sh
@@ -19,9 +19,8 @@ set -ex
 cp -r src src_copied
 cd src_copied
 
-
-conda create -n build python=3.10 -y
 conda init
+conda create -n build python=3.10 -y
 conda activate build
 
 python3 setup.py bdist_wheel --lite
